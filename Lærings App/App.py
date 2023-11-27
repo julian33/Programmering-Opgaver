2811d418262c07e752b2e5a381a162567435d14a
julian33 <j.nordbred@gmail.com>
2023-11-27 14:31:29 +0100

    App works now and has the factory method implemented

--- Lærings App/App.py
+++ Lærings App/App.py
@@ -1,19 +1,134 @@
 import tkinter as tk
+import random
+#import gradio
 
-def on_button_click():
-    label.config(text="Button Clicked!")
 
-# Create the main window
+
+# Klasse til at oprette knapper
+class MathButtons:
+    def __init__(self, root, text, command):
+        self.button = tk.Button(root, text=text, command=command)
+        self.button.pack(pady=10)
+
+    def destroy(self):
+        self.button.destroy()
+
+# Klasse til at generere spørgsmål
+class QuestionFactory:
+    def generate_question(self):
+        pass
+
+# Underklasse til addition
+class AdditionFactory(QuestionFactory):
+    def generate_question(self):
+        num1, num2 = self.generate_numbers()
+        question = f"What is {num1} + {num2}?"
+        answer = num1 + num2
+        return question, answer
+
+    def generate_numbers(self):
+        return random.randint(-10, 10), random.randint(-10, 10)
+
+# Underklasse til subtraktion  
+class SubtractionFactory(QuestionFactory):
+    def generate_question(self):
+        num1, num2 = self.generate_numbers()
+        num1, num2 = max(num1, num2), min(num1, num2)
+        question = f"What is {num1} - {num2}?"
+        answer = num1 - num2
+        return question, answer
+
+    def generate_numbers(self):
+        return random.randint(-10, 10), random.randint(-10, 10)
+
+# Underklasser til multiplikation
+class MultiplicationFactory(QuestionFactory):
+    def generate_question(self):
+        num1, num2 = self.generate_numbers()
+        question = f"What is {num1} * {num2}?"
+        answer = num1 * num2
+        return question, answer
+
+    def generate_numbers(self):
+        return random.randint(-10, 10), random.randint(-10, 10)
+
+# Underklasser til division
+class DivisionFactory(QuestionFactory):
+    def generate_question(self):
+        num1, num2 = self.generate_numbers()
+        if num2 == 0:
+            num1, num2 = self.generate_numbers()
+        result = num1 // num2
+        question = f"What is {num1} / {num2}?"
+        answer = result
+        return question, answer
+
+    def generate_numbers(self):
+        return random.randint(-10, 10), random.randint(-10, 10)
+
+# Funktioner til at fjerne eksisterende knapper
+def destroy_choices():
+    for math_button in math_choice_buttons:
+        math_button.destroy()
+
+# Funktion til at fjerne svarknapper
+def destroy_answers():
+    for math_button in math_answer_buttons:
+        math_button.destroy()
+
+# Funktion til at håndtere valg af matematisk operation 
+def on_choice_chosen(factory):
+    destroy_choices()
+    destroy_answers()
+
+    # Genererer et nyt spørgsmål baseret på den valgte matematiske operation
+    question, correct_answer = factory.generate_question()
+    label.config(text=question)
+
+    # Genererer tre forkerte svar
+    incorrect_answers = [correct_answer + random.randint(-5, 5) for _ in range(3)]
+    answers = [correct_answer] + incorrect_answers
+    random.shuffle(answers)
+
+    # Opretter knapper til svarmuligheder
+    for answer in answers:
+        math_answer_buttons.append(MathButtons(root, str(answer), lambda a=answer: check_answer(a, correct_answer, factory)))
+
+# Funktion til at kontrollere det valgte svar
+def check_answer(selected_answer, correct_answer, factory):
+    if selected_answer == correct_answer:
+        label.config(text="Correct!")
+    else:
+        label.config(text=f"Wrong! Correct answer is {correct_answer}")
+    root.after(3000, lambda: reset_question(factory))
+
+# Funktion til at gå til næste spørgsmål
+def reset_question(factory):
+    destroy_choices()
+    destroy_answers()
+    on_choice_chosen(factory)
+
+# Oprettelse af skærmen
 root = tk.Tk()
-root.title("Simple Tkinter UI")
+root.title("Matematik Træneren")
 
-# Create a label to display text
-label = tk.Label(root, text="Hello, Tkinter!")
+# Størrelse på vinduet
+root.geometry("960x540")  # Width x Height
+
+# Oprettelse af et label til tekstvisning
+label = tk.Label(root, text="Vælg matematik emne!")
 label.pack(pady=10)
 
-# Create a button
-button = tk.Button(root, text="Click Me!", command=on_button_click)
-button.pack(pady=10)
+# Oprettelse af knapper til valg af matematiske emne
+math_choice_buttons = [
+    MathButtons(root, "Addering", lambda: on_choice_chosen(AdditionFactory())),
+    MathButtons(root, "Substrahering", lambda: on_choice_chosen(SubtractionFactory())),
+    MathButtons(root, "Multiplicering", lambda: on_choice_chosen(MultiplicationFactory())),
+    MathButtons(root, "Dividering", lambda: on_choice_chosen(DivisionFactory())),
+]
 
-# Run the Tkinter event loop
+# Liste til at gemme knapper til svarmuligheder
+math_answer_buttons = []
+
+# Kører Tkinter event loop
 root.mainloop()
